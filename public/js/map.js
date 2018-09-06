d2e7404879d13dbf86b01347402e48649f79ab43
Nicole Cooper-White <nwhite1018@gmail.com>
2018-09-05 20:17:30 -0600

    working on map feature

---
+++ public/js/map.js
@@ -0,0 +1,41 @@
+var baseCoords = []
+
+var mymap = L.map("mapid").setView([51.505, -0.09], 13);
+
+L.tileLayer(
+  "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibmNvb3BlcnciLCJhIjoiY2pscHYybHlqMjkybDNrb3NuODQ3enhkdyJ9.PYpALCFWSGmaKkhN35TpwA",
+  {
+    attribution:
+      "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    minZoom: 13,
+    id: "mapbox.streets",
+    accessToken:
+      "pk.eyJ1IjoibmNvb3BlcnciLCJhIjoiY2pscHYybHlqMjkybDNrb3NuODQ3enhkdyJ9.PYpALCFWSGmaKkhN35TpwA"
+  }
+).addTo(mymap);
+
+//Show my current location
+var myLoc = mymap.locate({
+  setView: true,
+  maxZoom: 16,
+  enableHighAccuracy: true
+});
+
+function onLocationFound(e) {
+  var currentMarker = L.marker(e.lating, {
+    icon: currentLocIcon
+  });
+  currentMarker.addTo(mymap);
+}
+function onLocationError(e) {
+  alert(e.message);
+}
+mymap.on("locationerror", onLocationError);
+
+// $(function() {
+//   mymap.setView(baseCoords, 15);
+//   $.get("/api/trucks").then(function(data) {
+
+//   });
+// });
