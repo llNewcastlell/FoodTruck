ba2f36ac641c0614b14353f7ae23e4502e07665d
Nicole Cooper-White <nwhite1018@gmail.com>
2018-09-09 20:27:41 -0600

    truck search styling

--- public/js/truckSearch.js
+++ public/js/truckSearch.js
@@ -17,84 +17,50 @@ $(document).ready(function() {
         searchRows.push(createFtRow(foodTruckdata[i]));
       }
 
-      // console.log(foodTruckdata);
-      // console.log("inside truck search");
+      console.log(foodTruckdata);
+      console.log("inside truck search");
     });
   });
-});
 
-//Function to print foodtrucks in a row
-function createFtRow(foodTruckData) {
-  var newTr = $("<tr>");
-  newTr.data("name", foodTruckData);
-  newTr.append("<td>" + foodTruckData.name + "</td>");
-  newTr.append("<td>" + foodTruckData.foodType + "</td>");
-  newTr.append("<td>" + foodTruckData.description + "</td>");
-  newTr.append("<td><a href='" + foodTruckData.menuLink + "'>Menu</a></td>");
-  newTr.append("<td>" + foodTruckData.price + "</td>");
-  newTr.append("<td>" + foodTruckData.phone + "</td>");
-  console.log(newTr);
-  $(".truckContainer").html(newTr);
-  return newTr;
-}
-
-function renderFoodTruckList(rows) {
-  truckList
-    .children()
-    .not(":last")
-    .remove();
-  truckContainer.children(".alert").remove();
-  if (rows.length) {
-    console.log(rows);
-    truckList.prepend(rows);
-  } else {
-    renderEmpty();
+  //Function to print foodtrucks in a row
+  function createFtRow(foodTruckData) {
+    var newTr = $("<tr>");
+    newTr.data("name", foodTruckData);
+    newTr.append("<td>" + foodTruckData.name + "</td>");
+    newTr.append("<td>" + foodTruckData.foodType + "</td>");
+    newTr.append("<td>" + foodTruckData.description + "</td>");
+    newTr.append("<td><a href='" + foodTruckData.menuLink + "'>Menu</a></td>");
+    newTr.append("<td>" + foodTruckData.price + "</td>");
+    newTr.append("<td>" + foodTruckData.phone + "</td>");
+    console.log(newTr);
+    $(".truckContainer").html(newTr);
+    return newTr;
   }
-}
 
-//   Function renderEmpty(){
-//     var alertDiv = $("<div>");
-//     alertDiv.addClass("alert alert-danger");
-//     alertDiv.text("No Food Trucks at this time. Try again soon");
-//     authorContainer.append(alertDiv);
-//   };
-
-// refreshfoodTrucks gets new foodTrucks from the db and repopulates the list
-function getFoodTrucks() {
-  return $.ajax({
-    url: "/api/foodTruck/:foodType",
-    type: "GET"
-  }).then(function(data) {
-    var $foodTrucks = data.map(function(foodTruck) {
-      var $a = $("<a>")
-        .text(foodTruck.text)
-        .attr("href", "/foodTruck/" + foodTruck.id);
-      var $li = $("<li>")
-        .attr({
-          class: "list-group-item",
-          "data-id": foodTruck.id
-        })
-        .append($a);
-      var $button = $("<button>")
-        .addClass("btn btn-danger float-right delete")
-        .text("ｘ");
-      $li.append($button);
-      return $li;
-    });
-    $foodTruckList.empty();
-    $foodTruckList.append($foodTrucks);
-  });
-}
-
-//
-// //find all trucks with the foodtype clicked and show the results in the #results div as a list
-// db.FoodTruck.findAll({
-//     where: {
-//         foodType: search
-//     }
-// }).then(function (FoodTruck) {
-//     res.json(FoodTruck);
-//     console.log(FoodTruck);
-//     // truckResults(FoodTruck);
-// })
-//});
+  // refreshfoodTrucks gets new foodTrucks from the db and repopulates the list
+  // function getFoodTrucks(foodType) {
+  //   return $.ajax({
+  //     url: "/api/foodTruck/" + foodType,
+  //     type: "GET"
+  //   }).then(function(data) {
+  //     var $foodTrucks = data.map(function(foodTruck) {
+  //       var $a = $("<a>")
+  //         .text(foodTruck.text)
+  //         .attr("href", "/foodTruck/" + foodTruck.id);
+  //       var $li = $("<li>")
+  //         .attr({
+  //           class: "list-group-item",
+  //           "data-id": foodTruck.id
+  //         })
+  //         .append($a);
+  //       var $button = $("<button>")
+  //         .addClass("btn btn-danger float-right delete")
+  //         .text("ｘ");
+  //       $li.append($button);
+  //       return $li;
+  //     });
+  //     $foodTruckList.empty();
+  //     $foodTruckList.append($foodTrucks);
+  //   });
+  // }
+});
