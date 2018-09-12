9760ded3b229d3d44d4f1feffec16638da586872
Tessa Williams <tessawilliams@Tessas-MacBook-Pro.local>
2018-09-11 18:13:49 -0600

    review page added and connected to db

--- controllers/forageController.js
+++ controllers/forageController.js
@@ -3,6 +3,8 @@ var express = require("express");
 var router = express.Router();
 
 var foodtruck = require("../models/foodTruck.js");
+
+var review = require("../models/review.js");
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
   foodtruck.all(function(data) {
@@ -12,6 +14,11 @@ router.get("/", function(req, res) {
     console.log("********" + hbsObject);
     res.render("index", hbsObject);
   });
+});
+
+router.post("/review", function(req, res) {
+  // review.create(req.body);
+  console.log(req.body);
 });
 
 // Export routes for server.js to use.
