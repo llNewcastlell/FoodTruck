aa15635ae5724000a1e98c548bd2019d0ea402fb
Nicole Cooper-White <nwhite1018@gmail.com>
2018-09-13 21:27:20 -0600

    resolving conflicts

--- public/js/truckSearch.js
+++ public/js/truckSearch.js
@@ -77,17 +77,10 @@ $(document).ready(function() {
     mymap.on("click", onMapClick);
 
     L.tileLayer(
-<<<<<<< HEAD
       "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
       {
         attribution:
           'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-=======
-      "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-
-        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-
->>>>>>> master
         maxZoom: 18,
         minZoom: 13,
         id: "mapbox.emerald",
